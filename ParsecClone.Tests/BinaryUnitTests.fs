dec7a8421e73119d8f571690c0df763f1cdb0b18
Henrik Feldt <henrik@haf.se>
2018-01-21 20:07:30 +0100

    Unit tests all passing

--- ParsecClone.Tests/BinaryUnitTests.fs
+++ ParsecClone.Tests/BinaryUnitTests.fs
@@ -47,7 +47,7 @@ let binTest2() =
 
   let result = test parserStream parser
 
-  byteListToArr result |> Expect.equal "equal" bytes
+  byteListToArr result |> Expect.equal "equal" (Array.take 4 bytes)
 
 let binTest3() =
   let bytes = [|0;1;2;3;4;5;6;7;8|] |> Array.map byte
